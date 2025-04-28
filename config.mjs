dc5d2a205e24115a380e1ffadeed068f95106e42
Hayong Yang <ssk04022@gmail.com>
2025-04-28 15:42:57 +0900

    환경변수 추가

---
+++ config.mjs
@@ -0,0 +1,26 @@
+// .env에 있는 파일을 읽어오는 곳
+
+import dotenv from "dotenv";
+
+dotenv.config(); // .env를 싹 읽어옴
+
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  if (value == null) {
+    throw new Error(`키 ${key}는 undefined!!`);
+  }
+  return value;
+}
+
+export const config = {
+  jwt: {
+    secretKey: required("JWT_SECRET"),
+    expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 86400)),
+  },
+  bcrypt: {
+    saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 10)),
+  },
+  host: {
+    port: parseInt(required("HOST_PORT", 8080)),
+  },
+};
